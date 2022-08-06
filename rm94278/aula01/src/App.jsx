5083e1d98689ef01e41dd17b58303b91974a1a77
leo10k <leozinhomacedo41@gmail.com>
2022-08-05 21:12:22 -0300

    Primeiro App

---
+++ rm94278/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+    const tag = <strong>Ola React!</strong>
+    return(
+        <div>
+            <h1>{tag}</h1>
+            <h2>Subtitulo</h2>
+        </div>
+    )
+}
