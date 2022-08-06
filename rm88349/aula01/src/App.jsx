de9a5ec3b293c8bc72bedb4fb20fa176469bbf6a
luckgon <rm88349@fiap.com.br>
2022-08-05 21:14:39 -0300

    Primeiro App

---
+++ rm88349/aula01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App(){
+    const tag = <strong>Olá React</strong>
+    
+    return(
+        <div>
+            <h1>{tag}</h1>
+        </div>
+    )
+
+}
+
