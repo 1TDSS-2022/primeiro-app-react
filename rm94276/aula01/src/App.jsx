3fec7b6ef31132b558544e74c3411f0a778b0e8b
HenriqueCesarS <rm94276@fiap.com.br>
2022-08-06 00:19:49 -0300

    Versão01

---
+++ rm94276/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+export default function App(){
+    const tag = "Olá React!"
+    return(
+        <>
+            <h1>{tag}</h1>
+            <h2>REALIZADO POR HENRIQUE CESAR</h2> 
+        </>
+    )
+}
