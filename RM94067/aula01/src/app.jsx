63c32abd192eab4d83cbb52a191fef9c737545a7
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2022-08-05 21:15:23 -0300

    criando react e ERRO

---
+++ RM94067/aula01/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App(){
+    const tag = <strong>Hello react!</strong>
+    return(
+        <div>
+        <h1>{tag}</h1>
+        <h2>Subitítulo</h2>
+        </div>
+    )
+}
---
+++ RM94067/aula01/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import reactDOM from 'react-DOM'
+import App from './app'
+
+
+reactDOM.render(
+    <App/>, document.getElementById('root'))
