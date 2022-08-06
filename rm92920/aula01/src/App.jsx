d654a5b328c5865b9c8d61fca1343b03be6635ad
JosephGabriells <rm92920@fiap.com.br>
2022-08-05 21:15:29 -0300

    Primeiros passos em React

---
+++ rm92920/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function App(){
+    const tag = <strong>Olá React!</strong>
+    return(
+        <div>
+        <h1>{tag}</h1>
+        <h2>Eita que troço doido</h2>
+        </div>
+    )
+}
