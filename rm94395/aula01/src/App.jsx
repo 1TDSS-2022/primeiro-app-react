f4c0ac78e97fc6ec1e64271a058a57e73fc3faab
Samuel01111 <rm94395@fiap.com.br>
2022-08-05 21:14:35 -0300

    first commit using jsx. VROM to ROM

---
+++ rm94395/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+    const tag = <strong><h1>"Olá React"</h1></strong>
+    return (
+        <div>
+            <h1>{tag}</h1>
+            <h2>Subtitulo</h2>
+        </div>
+    )
+}
