b59c94a2dce089384de82763cbd04b707b6a8aa1
luan.reis <luan.reis@portoseguro.com.br>
2022-08-05 21:21:41 -0300

    Primeira app react

---
+++ rm94898/aula01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App(){
+    const tag = <strong>Olá Mundo</strong>
+    return(
+        <div>
+            <h1>{tag}</h1>
+            <h2>Teste</h2>
+        </div>
+    
+
+    )
+}
