69adda44ab959217f22e2b28073405e35fb25863
JosephGabriells <rm92920@fiap.com.br>
2022-08-10 20:10:58 -0300

    Pro Jow

--- rm92920/aula01/src/App.jsx
+++ rm92920/aula01/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react"
+import './App.css'
 
 export default function App(){
     const tag = <strong>Olá React!</strong>
     return(
+        <>
         <div>
         <h1>{tag}</h1>
         <h2>Eita que troço doido</h2>
         <h2>Coisa de corno</h2>
         </div>
+        </>
+
     )
 }
