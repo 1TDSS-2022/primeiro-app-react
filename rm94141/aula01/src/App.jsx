ab3ff0fffd14438c80a2abd2188bdb4a915205da
mariananieton <rm94141@fiap.com.br>
2022-08-10 16:44:28 -0300

    Primeiro App

---
+++ rm94141/aula01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App(){
+    const tag = <strong>Olá React!</strong>
+    return(
+        <div>
+        <h1>{tag}</h1>
+        <h2>Subtitulo</h2>
+        </div>
+    )
+}
