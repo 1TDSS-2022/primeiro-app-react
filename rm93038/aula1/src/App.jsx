276b6d94125c4fff638fc846e44ae276db1bcc52
Breno <rm93038@fiap.com.br>
2022-08-10 15:24:59 -0300

    Primeiro App

---
+++ rm93038/aula1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App(){
+    const tag = <strong>"Olá React!"</strong>
+    return (
+        <div>
+            <h1>{tag}</h1>
+            <h2>Subtítulo</h2>
+        </div>
+    )
+}
