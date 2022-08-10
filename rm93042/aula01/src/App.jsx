f40c67c1088b735f6f7d091da73ea387482a915c
FilipeJava <RM93042@fiap.com.br>
2022-08-10 13:45:52 -0300

    introducao ao React

---
+++ rm93042/aula01/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// criar estrutura do componente--- assinatura (APP nome)
+
+export default function App(){
+    const tag = <strong>Olá React!!!</strong>
+        return(
+            <div>
+                <h1>{tag}</h1>
+                <h2>Subtítulo</h2>
+            </div>
+            
+        )
+}
