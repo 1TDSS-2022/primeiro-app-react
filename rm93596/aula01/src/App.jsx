efe9783ee0d756f07590adeed0c96094076cf8bc
EduardaVigo <rm93596@fiap.com.br>
2022-08-10 21:03:22 -0300

    REAGEEEEEE

--- rm93596/aula01/src/App.jsx
+++ rm93596/aula01/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import './App.css'
+import Cabecalho from './components/cabecalho/cabecalho'
+import Carros from './components/carros/Carros'
+import ListaDeAlunos from './components/listaDeAlunos/ListaDeAlunos'
 
 export default function App() {
+    const aluno = "Luan"
+    const turma = "1TDSS"
+    const profs = ["Thiago", "Gondo", "Cordeiro", "Miguel", "Spinola"]
     const tag = <strong>Olá React!</strong>
     return (
         <div>
-            <h1>{tag}</h1>
-            <h2>Subtítulo</h2>
+           
+            <h2>INFORMAÇÕES</h2>
+            <ListaDeAlunos
+            alunoProp={aluno}
+            turmaProp={turma}
+            profsProp={profs}
+            />
+
         </div>
     )
 }
---
+++ rm93596/aula01/src/components/listaDeAlunos/ListaDeAlunos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function ListaDeAlunos(props) {
+  return (
+    <div>
+        <h2>Nome aluno: {props.alunoProp}</h2>
+        <h2>Nome turma: {props.turmaProp}</h2>
+        <h2>Professores: </h2>
+        <ul>
+            <li>{props.profsProp[0]}</li>
+            <li>{props.profsProp[1]}</li>
+            <li>{props.profsProp[2]}</li>
+            <li>{props.profsProp[3]}</li>
+            <li>{props.profsProp[4]}</li>
+        </ul>
+    </div>
+  )
+}
