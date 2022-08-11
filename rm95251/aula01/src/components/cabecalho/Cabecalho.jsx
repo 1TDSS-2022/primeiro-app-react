aa028734533fa36a530519f598294b5f3b11fde5
wallisonfer <wallisonsp.santos@gmail.com>
2022-08-10 21:03:53 -0300

    Componentes filhos / PROPS

---
+++ rm95251/aula01/src/components/cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Cabecalho(){
+    return(
+        <div>
+        <header>
+            Minha aplicação REACT!
+            <p>Iniciando componentes filho</p>
+        </header>
+        </div>
+    )   
+}
--- rm95251/aula01/src/index.js
+++ rm95251/aula01/src/index.js
@@ -6,5 +6,5 @@ ReactDom.render(
   <App/>,
   document.getElementById('root'))
 
-
+ 
 
