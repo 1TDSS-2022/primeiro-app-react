23ee97e2d61ae2937b17ae4c46ab109742334532
mariananieton <rm94141@fiap.com.br>
2022-08-15 11:50:27 -0300

    Componentes Filhos / PROPS

--- rm94141/aula01/src/App.jsx
+++ rm94141/aula01/src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from "react"
+import './App.css'
+import Cabecalho from "./components/cabecalho/Cabecalho"
+import Carros from "./components/carros/Carros"
+import ListaDeAlunos from "./components/listaDeAlunos/ListaDeAlunos"
 
 export default function App(){
+
+    const aluno = "Luan"
+    const turma = "1TDSS"
+    const profs = ["Thiago", "Gondo", "Cordeiro", "Miguel", "Salvio"]
+
     const tag = <strong>Olá React!</strong>
+
     return(
         <div>
-        <h1>{tag}</h1>
-        <h2>Subtitulo</h2>
+
+            <h2>INFORMAÇÕES</h2>
+            <ListaDeAlunos
+            alunoProp={aluno}
+            turmaProp={turma}
+            profsProp={profs}
+            />
         </div>
     )
 }
