48fa1b50e042adb85100cab225737d00ea68d3a4
luckgon <rm88349@fiap.com.br>
2022-08-17 19:23:23 -0300

    react

---
+++ rm88349/aula03/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+    
+  const paragEstilo = {
+    color: 'red',
+    fontSize: '1.8em',
+    fonFamily: 'Courrier New',
+    textIndent: '58px',
+    fontStyle: 'italic'
+
+
+  }
+  
+  return(
+    <div>
+      <h1>Estudos sobre state</h1>
+    </div>
+
+
+  )
+
+
+}
